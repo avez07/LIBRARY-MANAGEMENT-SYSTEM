b742927fc8b472dc2a25d9a579104259ac5ac6a8
avez07 <ansariaavez12345@gmail.com>
2023-01-31 05:04:31 +0530

    js created

---
+++ script.js
@@ -0,0 +1,13 @@
+function showform(){
+ var b = document.getElementsByClassName('form-f')
+    b[0].style.display = 'none'
+
+
+ var a = document.getElementsByClassName('login-form')
+ for(i = 0;i< b.length; i++){
+    a[i].style.display = 'block'
+
+
+}
+
+}
