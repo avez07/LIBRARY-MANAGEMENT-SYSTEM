ec9dce042a0f8f4ec1636587de50bd6569121335
avez07 <ansariaavez12345@gmail.com>
2023-01-31 21:18:00 +0530

    login form completed

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 function showform(){
- var b = document.getElementsByClassName('form-f')
-    b[0].style.display = 'none'
+ var b = document.getElementById('frist-form');
+    b.style.display = 'none';
 
+ var head = document.getElementsByClassName('login-form');
+ for(i = 0; i < head.length; i++){
+   head[i].style.display = 'block';
 
- var a = document.getElementsByClassName('login-form')
- for(i = 0;i< b.length; i++){
-    a[i].style.display = 'block'
-
-
+ }
 }
+function signup(){
+   var b = document.getElementById('frist-form');
+      b.style.display = 'none';
+  
+   var head = document.getElementsByClassName('signup-form');
+   for(i = 0; i < head.length; i++){
+     head[i].style.display = 'block';
+  
+   }
+  }
+  function login(){
+   var b = document.getElementsByClassName('signup-form');
+   for(i = 0; i < b.length; i++){
+      b[i].style.display = 'none';
+   }
+  
+   var head = document.getElementsByClassName('login-form');
+   for(i = 0; i < head.length; i++){
+     head[i].style.display = 'block';
+  
+   }
+  }
 
-}
