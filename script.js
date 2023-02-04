c859a16077bf29bfcf7f551d14d43f6e745ed2d0
avez07 <ansariaavez12345@gmail.com>
2023-02-04 18:43:56 +0530

    landing page passward  hide show function completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showform(){
  var b = document.getElementById('frist-form');
-    b.style.display = 'none';
+    b.style.display = 'none';                                       //show login form on click function
 
  var head = document.getElementsByClassName('login-form');
  for(i = 0; i < head.length; i++){
@@ -8,11 +8,13 @@ function showform(){
 
  }
 }
+
+
 function signup(){
    var b = document.getElementById('frist-form');
       b.style.display = 'none';
   
-   var head = document.getElementsByClassName('signup-form');
+   var head = document.getElementsByClassName('signup-form');               //show signup  form on click function         
    for(i = 0; i < head.length; i++){
      head[i].style.display = 'block';
   
@@ -32,22 +34,35 @@ function signup(){
   }
   function password(){
    var pass = document.getElementsByClassName('form-input')
+   var hide = document.getElementsByClassName('fa-eye')
+   var show = document.getElementsByClassName('fa-eye-slash')                                    //password hide & show on mouseup and doun function 
    if(pass[2].type === "password"||pass[9].type === "password"||pass[10].type === "password"){
       pass[2].type = "text";
       pass[9].type = "text";
       pass[10].type = "text";
+      
+    
+       for(i = 0; i < hide.length; i++){
+         hide[i].style.display = 'none';
+       }
+       for(i = 0; i < show.length; i++){
+         show[i].style.display = 'inline';
 
-      document.getElementById('eye-1').style.display = 'none';
-      document.getElementById('eye-2').style.display = 'inline';
-   }
+       }
+         }
    else{
       pass[2].type = "password";
       pass[9].type = "password";
       pass[10].type = "password";
-      document.getElementById('eye-2').style.display = 'none';
-      document.getElementById('eye-1').style.display = 'inline';
+      var hides = document.getElementById('fa-eye-slash')
+     var shows = document.getElementById('fa-eye')
+     for(i = 0; i < hide.length; i++){
+      hide[i].style.display = 'inline';
+    }
+    for(i = 0; i < show.length; i++){
+      show[i].style.display = 'none';
 
-      
+    }
    }
   }
  
