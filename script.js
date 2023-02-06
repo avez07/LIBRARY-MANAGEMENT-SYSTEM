7e37d3a5e071ce0e3e108aabd2fb8eb3ed347468
avez07 <ansariaavez12345@gmail.com>
2023-02-07 01:23:08 +0530

    all form re-completed according to requrement validation remain

--- script.js
+++ script.js
@@ -1,72 +1,41 @@
-function showform(){
- var b = document.getElementById('frist-form');
-    b.style.display = 'none';                                       //show login form on click function
-
- var head = document.getElementsByClassName('login-form');
- for(i = 0; i < head.length; i++){
-   head[i].style.display = 'block';
-
- }
+function login(){
+  window.location.assign("/login.html");
+return false
 }
-
-
 function signup(){
-   var b = document.getElementById('frist-form');
-      b.style.display = 'none';
-  
-   var head = document.getElementsByClassName('signup-form');               //show signup  form on click function         
-   for(i = 0; i < head.length; i++){
-     head[i].style.display = 'block';
-  
-   }
-  }
-  function login(){
-   var b = document.getElementsByClassName('signup-form');
-   for(i = 0; i < b.length; i++){
-      b[i].style.display = 'none';
-   }
-  
-   var head = document.getElementsByClassName('login-form');
-   for(i = 0; i < head.length; i++){
-     head[i].style.display = 'block';
-  
-   }
-  }
-  function password(){
-   var pass = document.getElementsByClassName('form-input')
-   var hide = document.getElementsByClassName('fa-eye')
-   var show = document.getElementsByClassName('fa-eye-slash')                                    //password hide & show on mouseup and doun function 
-   if(pass[2].type === "password"||pass[9].type === "password"||pass[10].type === "password"){
-      pass[2].type = "text";
-      pass[9].type = "text";
-      pass[10].type = "text";
-      
-    
-       for(i = 0; i < hide.length; i++){
-         hide[i].style.display = 'none';
-       }
-       for(i = 0; i < show.length; i++){
-         show[i].style.display = 'inline';
-
-       }
-         }
-   else{
-      pass[2].type = "password";
-      pass[9].type = "password";
-      pass[10].type = "password";
-      var hides = document.getElementById('fa-eye-slash')
-     var shows = document.getElementById('fa-eye')
-     for(i = 0; i < hide.length; i++){
-      hide[i].style.display = 'inline';
-    }
-    for(i = 0; i < show.length; i++){
-      show[i].style.display = 'none';
-
-    }
-   }
-  }
- 
-     
+  window.location.assign("/signup.html");
+  return false
+}
+function password(){
+  var x = document.getElementsByClassName("log");
+  if (x[1].type === "password") {
+    x[1].type = "text";
+    document.getElementById("eye-1").style.display = "none";
+    document.getElementById("eye-2").style.display = "inline";
    
- 
+  } 
+   else {
+    x[1].type = "password";
+    document.getElementById("eye-1").style.display = "inline";
+    document.getElementById("eye-2").style.display = "none";
+  }
+  }
+  function password2(){
+    var y = document.getElementsByClassName("sign");
+    if (y[3].type === "password" || y[4].type === "password") {
+      y[3].type = "text";
+      y[4].type = "text";
 
+      document.getElementById("eye-1").style.display = "none";
+      document.getElementById("eye-2").style.display = "inline";
+     
+    } 
+     else {
+      y[3].type = "password";
+      y[4].type = "password";
+      document.getElementById("eye-2").style.display = "none";
+      document.getElementById("eye-1").style.display = "inline";
+      console.log(y[4])
+    }
+    }
+   
