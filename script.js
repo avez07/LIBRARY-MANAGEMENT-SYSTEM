ccea5fd259bd221b17987295f0133fe22d86e2ac
avez07 <ansariaavez12345@gmail.com>
2023-02-09 22:31:05 +0530

    signup validation completed

--- script.js
+++ script.js
@@ -21,13 +21,16 @@ function password(){
 
       document.getElementById("eye-1").style.display = "none";
       document.getElementById("eye-2").style.display = "inline";
-     
+      document.getElementById("eye-4").style.display = "none";
+      document.getElementById("eye-3").style.display = "inline";
     } 
      else {
       y[3].type = "password";
       y[4].type = "password";
       document.getElementById("eye-2").style.display = "none";
       document.getElementById("eye-1").style.display = "inline";
+      document.getElementById("eye-3").style.display = "none";
+      document.getElementById("eye-4").style.display = "inline";
      
     }
     }
@@ -37,54 +40,74 @@ function password(){
       var user = document.getElementById("username").value;
       var pass1 = document.getElementById("password1").value;
       var pass2 = document.getElementById("password2").value;
-    var paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
-      
-
+  
 
       if(name == null || name == ""){
-          document.getElementById("names").innerHTML = "please fill the text" ;
+          document.getElementById("names").innerHTML = "Please fill the text" ;
           return false;
       }
       else if(!isNaN(name)){
-        document.getElementById("names").innerHTML= "only characters is allowed";
+        document.getElementById("names").innerHTML= "Only characters is allowed";
         return false;
       }
       else if(email == null || email == ""){
-      document.getElementById("email-id").innerHTML = "please fill the text" ;
+      document.getElementById("email-id").innerHTML = "Please fill the text" ;
         return false;
     }
     else if (/[^a-z0-9@.]/.test(email)) {
-      document.getElementById("email-id").innerHTML="email contain only alphanumeric character";
+      document.getElementById("email-id").innerHTML="Email contain only Alphanumeric character";
         
         return false;
     }
     
     else if(email.indexOf('@') <= 0 || email.indexOf('.') <= 0){
-      document.getElementById('email-id').innerHTML = "@  '.' position are invalid ";
+      document.getElementById('email-id').innerHTML = "@  '.' Position are Invalid ";
       return false
     }
     
     else if(email.charAt(email.length-1) == "@" || email.charAt(email.length-1) == "."){
-      document.getElementById('email-id').innerHTML = "you canot type a @ or '.' in the end of email";
+      document.getElementById('email-id').innerHTML = "You canot type a @ or '.' in the end of email";
       return false
     }
     
     else if(email.charAt(email.length-4) != "." && email.charAt(email.length-3) != "."){
-      document.getElementById('email-id').innerHTML = "'.' position are invalid";
+      document.getElementById('email-id').innerHTML = "'.' Position are invalid";
       return false
     }
-   
-
     else if (user== null || user == ""){
-      document.getElementById("user").innerHTML = "please fill the text" ;
+      document.getElementById("user").innerHTML = "Please fill the text" ;
       return false
     }
     else if (pass1== null || pass1 == ""){
-      document.getElementById("pass1").innerHTML = "please fill the text" ;
+      document.getElementById("pass1").innerHTML = "Please fill the text" ;
       return false
     }
-    else if(/[^A-z]/.test(pass1)){
-      document.getElementById('pass1').innerHTML = "atleast one uppercase character is allowed"
+    else if(pass1[0] !== pass1[0].toUpperCase()){
+      document.getElementById('pass1').innerHTML = "Frist letter should be uppercase "
       return false
     }
-  }
+     else if(!/[a-z]/.test(pass1)){
+      document.getElementById('pass1').innerHTML = "Atleast one lowercase character"
+      
+      return false
+     }
+     else if(!/[0-9]/.test(pass1)){
+      document.getElementById('pass1').innerHTML = "Atleast one number"
+      
+      return false
+     }
+     else if(!/[ \W+]/.test(pass1)){
+      document.getElementById('pass1').innerHTML = "Atleast one special characters"
+      
+      return false
+     }
+     else if (pass2 == null || pass2 == ""){
+      document.getElementById('pass2').innerHTML = "Plese fill the text"
+      return false;
+     }
+     else if( pass1 != pass2){
+      document.getElementById('pass2').innerHTML = "Password don't match"
+      return false;
+     }
+
+    }
